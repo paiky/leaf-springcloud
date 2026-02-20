590e4d0692cdc487cca9c57bcac5caa4b68d5342
linpei <2859431743@qq.com>
2026-02-20 22:09:51 +0800

    chore: init root project and leaf-common module

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# Leaf SpringCloud Learning Notes
---
+++ leaf-common/src/main/java/com/leaf/common/result/Result.java
@@ -0,0 +1,54 @@
+package com.leaf.common.result;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * 统一网关、微服务 API 响应体
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Result<T> implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 响应业务状态码
+     */
+    private Integer code;
+
+    /**
+     * 响应信息
+     */
+    private String message;
+
+    /**
+     * 响应数据
+     */
+    private T data;
+
+    public static <T> Result<T> success() {
+        return success(null);
+    }
+
+    public static <T> Result<T> success(T data) {
+        return Result.<T>builder()
+                .code(200)
+                .message("Success")
+                .data(data)
+                .build();
+    }
+
+    public static <T> Result<T> fail(Integer code, String message) {
+        return Result.<T>builder()
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
